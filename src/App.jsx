c9ce238fa93f64f5ccd09766b702d324a8e4f26b
ritik kumar <kr.ritik1510@gmail.com>
2025-02-04 00:21:16 +0530

    fixing the both parts of email sorting process

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,50 @@
-import { useState } from 'react'
+import { useState, useRef, useEffect } from 'react'
 import './App.css'
 
 function App() {
   const [ text, setText ] = useState('Empty');
   let [ sortedEmails, setSortedEmails ] = useState(' ');
+  const textareaRef = useRef(null);
 
   let expression = /[\w\.-]+@[\w\.-]+\.[a-z]{2,}/gi;
-  
+
   const handleChange = (event) => {
     setText(event.target.value);
     const emails = text.match(expression);
     setSortedEmails(emails ? emails.join('\n') : 'No emails found');
   }
 
+  const handleInput = () => {
+    const textarea = textareaRef.current;
+    textarea.style.height = "auto"; // Reset height
+    textarea.style.height = textarea.scrollHeight + "px"; // Set new height
+  };
+
   return (
     <>
-      <div>
-        <label htmlFor="inputText">Enter unfilteres data : </label>
-        <input
-          placeholder='text'
-          id='inputText'
-          value={text}
-          onChange={handleChange} />
+      <div className='input-section'>
+        <div className="input-group">
+          <span className="input-group-text">Unsorted Emails:</span>
+          <textarea
+            className="form-control textarea-cln"
+            aria-label="With textarea"
+            value={text}
+            onChange={handleChange}></textarea>
+        </div>
+        <div className="input-group">
+          <span className="input-group-text">Sorted Emails:</span>
+          <textarea 
+            className="form-control textarea-cln" 
+            aria-label="With textarea" 
+            value={sortedEmails}
+            ref={textareaRef}
+            onChange={handleInput}
+            placeholder='Get Sorted Emails Here'>
+          </textarea>
+        </div>
       </div>
-      <label htmlFor="textarea">Textarea for sorted emails : </label>
       <div>
         <button id='getSortedEmails' onClick={handleChange}>Get Sorted Mails</button>
-      </div>
-      <div>
-        <h3>Sorted Emails:</h3>
-        <pre>{sortedEmails}</pre>
       </div>
     </>
   )
